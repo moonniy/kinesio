20629c8ad2146b3d89cae8e863e208b1791f7a30
moonniy <mony.23@hotmail.com>
2016-02-23 18:10:20 -0600

    Agregar componente footer y hero

---
+++ components/footer.js
@@ -0,0 +1,62 @@
+<footer>
+        <div class="footer gradient-2">
+            <div class="container footer-container ">
+                <div class="row">
+                    <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
+                        <div class="footer-title">Titulo</div>
+                        <ul class="list-unstyled">
+                            <li><a href="#"> title </a></li>
+                            <li><a href="#" type="application/atom+xml" rel="alternate">ATOM_LINK</a></li>                           
+                            <li><a href="#" type="application/rss+xml" rel="alternate">RSS_LINK</a></li>                           
+                        </ul>
+                    </div>
+                    <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
+                        <div class="footer-title">SOCIAL</div>
+                        <ul class="list-unstyled">
+                            <li><a href="#" target="_blank">name</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-4 col-sm-3 col-md-3 col-lg-3">
+                        <div class="footer-title">LINKS</div>
+                        <ul class="list-unstyled">
+                            <li><a href="#" target="_blank">name</a></li>
+                        </ul>
+                    </div> 
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                        <p class="pull-right text-right">
+                            <small><em>Proudly powered by <a href="http://docs.getpelican.com/" target="_blank">pelican</a></em></small><br/>
+                            <small><em>Theme and code by <a href="https://github.com/molivier" target="_blank">molivier</a></em></small><br/>
+                            <small>NEST_COPYRIGHT</small>
+                            <small><em>NEST_FOOTER_HTML</em></small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    <style>
+
+footer .footer{
+    background-color: #333;
+    font-size: 14px;
+    font-weight: 300;
+    text-align: left;
+    color: #fff;
+}
+
+footer .footer-title {
+    text-transform: uppercase;
+    margin-bottom: 3px;
+    font-weight: 600;
+    font-size: 12px;
+}
+footer .footer-container{
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+footer .footer a{
+    color: #fff;
+}
+    </style>
+</footer>
--- components/hero.js
+++ components/hero.js
@@ -1,10 +1,20 @@
 <hero>
+<div class="video-container">
 <video width="100%" autoplay  poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" loop>
   <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
 <source src="//demosthenes.info/assets/videos/polina.webm" type="video/webm">
 <source src="//demosthenes.info/assets/videos/polina.mp4" type="video/mp4">
     </video>
-    <style>
-    
-    </style>
+    <div class="description">
+       <h1>Short description of Product</h1>
+       <p>Chocolate cake fruitcake sweet gingerbread bonbon. Carrot cake soufflé candy canes apple pie.</p>
+    </div>
+</div>
+<style>
+hero .video-container{
+    position: relative;
+    text-align: center;
+}
+
+</style>
 </hero>
